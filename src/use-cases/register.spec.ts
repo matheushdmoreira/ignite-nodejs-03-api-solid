37edc9ebc332aafa3c935dc921c5bc20283674cb
Matheus Moreira <matheustjp111@gmail.com>
2025-01-21 08:48:09 -0300

    feat: authenticate use case

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
-import { UserAlreadyExistsError } from './errors/user-already-exists'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register Use Case', () => {
   it('should be able to register', async () => {
