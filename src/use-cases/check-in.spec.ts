5b4e45bd48467dfe47afb96aac52e24b73e99248
Matheus Moreira <matheustjp111@gmail.com>
2025-01-21 11:11:43 -0300

    feat: test and validate check in distance of gym

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -82,4 +82,24 @@ describe('Check In Use Case', () => {
 
     expect(checkIn.id).toEqual(expect.any(String))
   })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'Javascript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-22.8042865),
+      longitude: new Decimal(-47.0371793),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -22.8199121,
+        userLongitude: -47.0465349,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
 })
