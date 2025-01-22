6c17bb4bb3c85ce42be19fc202db7f6bc80f8533
Matheus Moreira <matheustjp111@gmail.com>
2025-01-22 10:02:52 -0300

    feat: get profile route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance } from 'fastify'
 
+import { verifyJwt } from './middlewares/verity-jwt'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  // Authenticated
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
